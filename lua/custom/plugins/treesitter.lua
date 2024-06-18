2e2a9cd0d9cd4a5880bee5216f8ee4b5f247d25f
Michael Goerz <mail@michaelgoerz.net>
2024-06-18 08:29:56 -0400

    Configure treesitter

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -4,26 +4,24 @@ return {
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
       },
       indent = {
-          enable = true
+        enable = true,
+      },
+      incremental_selection = {
+        enable = false,
+      },
+      textobjects = {
+        enable = true,
       },
     },
     config = function(_, opts)
-      -- Prefer git instead of curl in order to improve connectivity in some environments
       require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      -- TODO: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 }
