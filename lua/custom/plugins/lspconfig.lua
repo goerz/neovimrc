9ecb86668738503bb832aa1fe0295e007b73e137
Michael Goerz <mail@michaelgoerz.net>
2024-06-21 09:59:13 -0400

    Customize Julia LSP

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -2,14 +2,18 @@ return {
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
+      'williamboman/mason-lspconfig.nvim', -- allows mason-tool-installer to accept lspconfig package names
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       'j-hui/fidget.nvim',
     },
     config = function()
+
+      require('vim.lsp.log').set_format_func(vim.inspect)
+      -- This doesn't seem to work, but it would be nice to have a better log
+
       vim.api.nvim_create_autocmd('LspAttach', {
+
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
 
@@ -67,6 +71,7 @@ return {
           })
 
         end,
+
       })
 
       -- LSP servers and clients are able to communicate to each other what features they support.
@@ -86,122 +91,56 @@ return {
         }
      )
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+      -- LSPs and other tools can (but don't have to) be installed via Mason/Mason-Tool-Installer
       --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
-        julials = {
-          cmd = {
-            "julia",
-            "--startup-file=no",
-            "--history-file=no",
-            "-e",
-            [[
-            # Load LanguageServer.jl:
-            # Attempt to load from ~/.julia/environments/nvim-lspconfig
-            # with the regular load path as a fallback
-            ls_install_path = joinpath(
-              get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")),
-              "environments",
-              "nvim-lspconfig"
-            )
-            pushfirst!(LOAD_PATH, ls_install_path)
-            using LanguageServer
-            popfirst!(LOAD_PATH)
-            depot_path = get(ENV, "JULIA_DEPOT_PATH", "")\
-            project_path = let
-              dirname(
-                something(\n            ## 1. Finds an explicitly set project (JULIA_PROJECT)\n            Base.load_path_expand((\n                p = get(ENV, "JULIA_PROJECT", nothing);\n                p === nothing ? nothing : isempty(p) ? nothing : p\n            )),\n            ## 2. Look for a Project.toml file in the current working directory,\n            ##    or parent directories, with $HOME as an upper boundary\n            Base.current_project(),\n            ## 3. First entry in the load path\n            get(Base.load_path(), 1, nothing),\n            ## 4. Fallback to default global environment,\n            ##    this is more or less unreachable\n            Base.load_path_expand("@v#.#"),
-                )
-              )
-            end
-            @info "Running language server" VERSION pwd() project_path depot_path
-            server = LanguageServer.LanguageServerInstance(
-              stdin, stdout, project_path, depot_path
-            )
-            server.runlinter = true
-            run(server)
-            ]]
-          },
-          root_dir = function(fname)
-            local util = require'lspconfig.util'
-            local r = util.root_pattern 'Project.toml'(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-            return r
-          end,
-          on_attach = function(client, bufnr)
-            -- Disable automatic formatexpr since the LS.jl formatter isn't so nice.
-            vim.bo[bufnr].formatexpr = ''
-          end,
-        },
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = {
-                globals = { 'vim' },
-                -- disable = { 'missing-fields' }
-              },
-            },
-          },
-        },
-
-      }
-
-      -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
       --  other tools, you can run
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
       require('mason').setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+      require('mason-tool-installer').setup {
+        -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim?tab=readme-ov-file#configuration
+        ensure_installed = {
+          'lua_ls',  -- automatically installs tools for Lua language server
+          -- Julia language server is installed manually, so we keep it ouf of Mason
+          'stylua',
+        }
       }
-    end,
+
+      require('lspconfig').lua_ls.setup({
+        -- cmd = {...},
+        -- filetypes = { ...},
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            diagnostics = {
+              globals = { 'vim' },
+              -- disable = { 'missing-fields' }
+            },
+          },
+        },
+      })
+
+      local julia_ls_script = vim.fs.joinpath(vim.fn.stdpath('config'), "helpers", "julia_languageserver.jl")
+      require'lspconfig'.julials.setup({
+        cmd = {"julia", "--startup-file=no", "--history-file=no", julia_ls_script},
+        single_file_support = true,
+        on_attach = function(client, bufnr)
+          -- Disable automatic formatexpr since the LS.jl formatter isn't so nice.
+          vim.bo[bufnr].formatexpr = ''
+        end,
+        capabilities = capabilities,
+      })
+
+      -- TODO: pyright (?)
+
+    end,  -- end of config function
+
   },
 }
 -- vim: ts=2 sts=2 sw=2 et fdm=marker fmr={,} nofen
