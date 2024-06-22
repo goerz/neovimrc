1b5809a60f3ed406ff20b52822758a0019b7a2ff
Michael Goerz <mail@michaelgoerz.net>
2024-06-22 12:00:18 -0400

    Tweak statusline

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -70,7 +70,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'%F'},
+        lualine_c = {{"filename", path = 1}},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
