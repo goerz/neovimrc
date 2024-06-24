b2b35b99924e9bb7d885ad28310633a27d822683
Michael Goerz <mail@michaelgoerz.net>
2024-06-24 07:23:26 -0400

    Keep cursor at end of selection for SLIME

--- lua/custom/plugins/vim-slime.lua
+++ lua/custom/plugins/vim-slime.lua
@@ -6,7 +6,7 @@ return {
       vim.g.slime_no_mappings = 1
       vim.g.slime_python_ipython = 1
       vim.api.nvim_set_keymap('n', '<leader>s', ':SlimeSend<CR>', { noremap = true, silent = true, desc="Send to SLIME"}) -- normal mode
-      vim.api.nvim_set_keymap('x', '<leader>s', ":'<,'>SlimeSend<CR>", { noremap = true, silent = true, desc="Send to SLIME"}) -- visual mode
+      vim.api.nvim_set_keymap('x', '<leader>s', ":'<,'>SlimeSend<CR>'>", { noremap = true, silent = true, desc="Send to SLIME"}) -- visual mode
     end
   },
 }
