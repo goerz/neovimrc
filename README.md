7ba6c8f55f2715e1566591750a139fcceda8cec7
Michael Goerz <mail@michaelgoerz.net>
2024-06-30 11:37:13 -0400

    Add GP plugin to connect to ChatGPT

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # Neovim Configuration
 
-This configuration is intended to work with [Neovim][] only. It is a fresh
-restart from an [earlier configuration](https://github.com/goerz/vimrc) that
-works for both [Vim][] and [Neovim][]. Here, we go all-in on using Lua for the
-configuration an generally following the most modern best practices.
+This configuration is intended to work with [Neovim][] only. It is a fresh restart from an [earlier configuration](https://github.com/goerz/vimrc) that works for both [Vim][] and [Neovim][]. Here, we go all-in on using Lua for the configuration and generally following the most modern best practices.
 
-Clone this repository to
-[`$XDG_CONFIG_HOME/nvim`](https://neovim.io/doc/user/nvim.html#nvim-from-vim),
-generally `~/.config/nvim`:
+Clone this repository to [`$XDG_CONFIG_HOME/nvim`](https://neovim.io/doc/user/nvim.html#nvim-from-vim), generally `~/.config/nvim`:
 
     user@host:~/.config/> git clone https://github.com/goerz/neovimrc.git nvim
 
@@ -32,7 +27,7 @@ configuration for each plugin in the subfolder `./lua/custom/plugins/`.
 
 Run `:Lazy` to open the plugin manager.
 
-Run `:Lazy reload <plugin name>`
+Run `:Lazy reload <plugin name>`.
 
 
 ### Commenting
@@ -41,7 +36,7 @@ To comment out a line or block, use `,c ` in normal or visual mode.
 
 ## Indentation Guides
 
-The [Indent Blankline](https://github.com/lukas-reineke/indent-blankline.nvim) plugin provides useful indentation guides. These are off by default, but can be toggled on with `,i`.
+The [Indent Blankline](https://github.com/lukas-reineke/indent-blankline.nvim) plugin provides useful indentation guides. These are off by default but can be toggled on with `,i`.
 
 ### Git (fugitive)
 
@@ -50,16 +45,33 @@ Use `,gd` to "git diff" the current file in such a way that editing the fugitive
 Use `,ga` to stage the current file with all changes. Create a commit with `,gc`.
 
 
+### ChatGPT Integration
+
+Integration with [ChatGPT](https://chatgpt.com) or more specifically the [OpenAI API](https://platform.openai.com) is provided by the [GP.nvim](https://github.com/Robitx/gp.nvim) plugin.
+
+The ChatGPT functionality uses the `ctrl-g` prefix. Most importantly, `ctrl-g c` toggles on a Chat window in a vertical split.
+
+There is a custom `GPCheckGrammar` command tied to the shortcut `ctrl-g s` (GPT-3) or `ctrl-g shift-s` (GPT-4, better but more expensive) that performs grammar and spell checking in a diffed split.
+
+
+### Autocompletion
+
+cmp: ctrl-space activates, enter deactivates.
+
+
 ### Telescope
 
-Telescope is mapped to `CTRL-F`. Typing `CTRL-F` by itself shows the telescope selector after a timeout. The `CTRL-F` shortcut can also be combined with other keys to directly jump to some of the most common modules, e.g.
+Telescope is mapped to `ctrl-f`. Typing `ctrl-f` by itself shows the telescope selector after a timeout. The `ctrl-f` shortcut can also be combined with other keys to directly jump to some of the most common modules, e.g.
 
-* `CTRL-F F` - Find Files
-* `CTRL-F H` – Find vim help tags
-* `CTRL-F K` - Find Keymap
-* `CTRL-F D` - Find LSP diagnostics
-* `CTRL-F B` - Find Buffers
+* `ctrl-f f` - Find Files
+* `ctrl-f h` – Find vim help tags
+* `ctrl-f k` - Find Keymap
+* `ctrl-f d` - Find LSP diagnostics
+* `ctrl-f b` - Find Buffers
 
-etc. (use `CTRL-F K` to find all keymaps)
+etc. (use `ctrl-f k` to find all keymaps)
 
-Telescope replaces the [ctrlp.vim](https://github.com/kien/ctrlp.vim) plugin I was using with vim. `CTRL-P` directly searches for files. Any file can be opened in a vertical or horizontal split with `CTRL-V`/`CTRL-O V` and `CTRL-X`/`CTRL-O H`, respectively.
+Telescope replaces the [ctrlp.vim](https://github.com/kien/ctrlp.vim) plugin I was using with vim. `ctrl-p` directly searches for files. Any file can be opened in a vertical or horizontal split with `ctrl-v`/`ctrl-o v` and `ctrl-x`/`ctrl-o h`, respectively.
+
+
+### LSP
---
+++ lua/custom/plugins/gp.lua
@@ -0,0 +1,209 @@
+return {
+  {
+    "robitx/gp.nvim",
+    event = "VeryLazy",
+    config = function()
+
+      require("gp").setup(
+        {
+          openai_api_key = { "cat", os.getenv("HOME") .. "/.openai-api-key.txt"},
+          chat_confirm_delete = false,
+          chat_assistant_prefix = { "🤖:", "[{{agent}}]" },
+          chat_user_prefix = "🧑:",
+          chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>." },
+          command_auto_select_response = false,
+          agents = {
+            {
+              name = "ChatGPT4",
+              chat = true,
+              command = false,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are a general AI assistant.\n\n"
+                .. "Follow these instructions when responding:\n\n"
+                .. "- If you're unsure don't guess and say you don't know instead.\n"
+                .. "- Ask question if you need clarification to provide a better answer.\n"
+                .. "- Be as concise as possible, unless asked to elaborate.\n"
+                .. "- Assume that you are talking to an expert unix user comfortable with working on the command line.\n"
+                .. "- Assume that the user is an expert programmer. When generating code, only add necessary comments.\n"
+                .. "- Don't elide any code from your output if the answer requires coding.\n"
+                .. "- You are talking to a computational physicist.\n",
+            },
+            {
+              name = "ChatGPT3-5",
+              chat = true,
+              command = false,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-3.5-turbo-0125", temperature = 1.1, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are a general AI assistant.\n\n"
+                .. "Follow these instructions when responding:\n\n"
+                .. "- If you're unsure don't guess and say you don't know instead.\n"
+                .. "- Ask question if you need clarification to provide a better answer.\n"
+                .. "- Be as concise as possible, unless asked to elaborate.\n"
+                .. "- Assume that you are talking to an expert unix user comfortable with working on the command line.\n"
+                .. "- Assume that the user is an expert programmer. When generating code, only add necessary comments.\n"
+                .. "- Don't elide any code from your output if the answer requires coding.\n"
+                .. "- You are talking to a computational physicist.\n",
+            },
+            {
+              name = "CodeGPT4",
+              chat = false,
+              command = true,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are an AI working as a code editor.\n\n"
+                .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+                .. "START AND END YOUR ANSWER WITH:\n\n```",
+            },
+            {
+              name = "CodeGPT3-5",
+              chat = false,
+              command = true,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-3.5-turbo-0125", temperature = 0.8, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are an AI working as a code editor.\n\n"
+                .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+                .. "START AND END YOUR ANSWER WITH:\n\n```",
+            },
+          },
+          hooks = {
+            CheckGrammar = function(gp, params)
+              local system_prompt = require("plenary.strings").dedent([[
+
+                You are a professional copyeditor for non-fiction. Your job is to lightly edit
+                text for spelling, grammar, and punctuation. Your instructions are as follows:
+
+                Edited text should be in American English and follow modern style guides.
+                In addition, take into account the following in-house rules:
+
+                * Use the Oxford comma.
+                * Extra commas can sometimes be used for emphasis.
+                * It is perfectly fine to use they/them as a singular pronoun.
+                * Do not use em-dashes (—)
+                * En-dashes (–) should be surrounded by spaces
+                * Headings at level 2 or higher may use title case or sentence case. Be consistent.
+
+                Text can be in a markup language such as Markdown or LaTeX. Any markup
+                syntax must be preserved. The text may contain mathematical
+                formulas in LaTeX syntax, which must not be modified.
+
+              ]])
+              local system_prompt_extra = {
+                markdown = require("plenary.strings").dedent([[
+
+                Recognize the given text as Markdown, with syntax extensions for CommonMark and Pandoc.
+                In Markdown, text enclosed with backticks (`) is inline
+                code, which must not be modified. Fenced codeblocks
+                surrounded by multiple backticks or tildes must not be modified.
+                Inline mathematics can be enclosed in dollar signs or using double backticks
+                and should not be modified. DO NOT REMOVE LINEBREAKS.
+                Keep multiple blank lines, especially before headers.
+                If a link is on a line by itself, keep it like that.
+
+                ]]),
+              }
+              local system_prompt_post = require("plenary.strings").dedent([[
+
+                Keep the original structure and style of the text,
+                and make as few changes as possible to ensure the text is free
+                of spelling errors or grammatical mistakes and is well-written.
+                DO NOT REFLOW THE TEXT. DO NOT ADD OR REMOVE BLANK LINES.
+                THE OUTPUT MUST HAVE THE SAME NUMBER OF LINES AS THE INPUT TEXT.
+                Do not add any commentary.
+
+              ]])
+              local template = ""
+              .. "Edit the following text for spelling and grammar. "
+              .. "Respond with an edited version of the text only. "
+              .. "This concludes your instructions. "
+              .. "This is the text to edit:\n\n{{selection}}\n"
+              local filetype = vim.bo.filetype
+              if system_prompt_extra[filetype] ~= nil then
+                system_prompt = system_prompt .. system_prompt_extra[filetype] .. system_prompt_post
+              else
+                system_prompt = system_prompt .. system_prompt_post
+              end
+              vim.api.nvim_command("diffthis")
+              -- vim.api.nvim_out_write(vim.inspect(params)) -- DEBUG
+              local model = "gpt-3.5-turbo-0125"
+              for _, arg in ipairs(params.fargs) do
+                if arg:match("gpt") and arg:match("4") then
+                  model = "gpt-4o"
+                end
+              end
+              gp.Prompt(
+                params,
+                gp.Target.vnew(vim.bo.filetype),
+                nil, -- command will run directly without any prompting for user input
+                model,
+                template,
+                system_prompt
+              )
+              vim.api.nvim_create_autocmd('User', {
+                pattern = {'GpDone'},
+                callback = function()
+                  vim.api.nvim_command("diffthis")
+                end,
+                once = true,
+              })
+            end,
+          },
+        }
+      )  -- setup
+
+      local function keymapOptions(desc)
+        return {
+          noremap = true,
+          silent = true,
+          nowait = true,
+          desc = "GPT prompt " .. desc,
+        }
+      end
+
+      -- Chat commands
+      vim.keymap.set({"n", "i"}, "<C-g>c", "<cmd>GpChatToggle<cr>", keymapOptions("Toggle Chat"))
+      vim.keymap.set({"n", "i"}, "<C-g>f", "<cmd>GpChatFinder<cr>", keymapOptions("Chat Finder"))
+
+      vim.keymap.set("v", "<C-g>p", ":<C-u>'<,'>GpChatPaste<cr>", keymapOptions("Visual Chat Paste"))
+      vim.keymap.set("v", "<C-g>c", ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions("Visual Toggle Chat"))
+
+      -- Prompt commands
+      vim.keymap.set({"n", "i"}, "<C-g>r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
+      vim.keymap.set({"n", "i"}, "<C-g>a", "<cmd>GpAppend<cr>", keymapOptions("Append (after)"))
+      vim.keymap.set({"n", "i"}, "<C-g>b", "<cmd>GpPrepend<cr>", keymapOptions("Prepend (before)"))
+
+      vim.keymap.set("v", "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", keymapOptions("Visual Rewrite"))
+      vim.keymap.set("v", "<C-g>a", ":<C-u>'<,'>GpAppend<cr>", keymapOptions("Visual Append (after)"))
+      vim.keymap.set("v", "<C-g>b", ":<C-u>'<,'>GpPrepend<cr>", keymapOptions("Visual Prepend (before)"))
+      vim.keymap.set("v", "<C-g>i", ":<C-u>'<,'>GpImplement<cr>", keymapOptions("Implement selection"))
+
+      vim.keymap.set({"n", "i"}, "<C-g>gp", "<cmd>GpPopup<cr>", keymapOptions("Popup"))
+      vim.keymap.set({"n", "i"}, "<C-g>ge", "<cmd>GpEnew<cr>", keymapOptions("GpEnew"))
+      vim.keymap.set({"n", "i"}, "<C-g>gn", "<cmd>GpNew<cr>", keymapOptions("GpNew"))
+      vim.keymap.set({"n", "i"}, "<C-g>gv", "<cmd>GpVnew<cr>", keymapOptions("GpVnew"))
+
+      vim.keymap.set("v", "<C-g>gp", ":<C-u>'<,'>GpPopup<cr>", keymapOptions("Visual Popup"))
+      vim.keymap.set("v", "<C-g>ge", ":<C-u>'<,'>GpEnew<cr>", keymapOptions("Visual GpEnew"))
+      vim.keymap.set("v", "<C-g>gn", ":<C-u>'<,'>GpNew<cr>", keymapOptions("Visual GpNew"))
+      vim.keymap.set("v", "<C-g>gv", ":<C-u>'<,'>GpVnew<cr>", keymapOptions("Visual GpVnew"))
+
+      vim.keymap.set({"n", "i"}, "<C-g>x", "<cmd>GpContext<cr>", keymapOptions("Toggle Context"))
+      vim.keymap.set("v", "<C-g>x", ":<C-u>'<,'>GpContext<cr>", keymapOptions("Visual Toggle Context"))
+
+      -- vim.keymap.set({"n", "i", "v", "x"}, "<C-g>s", "<cmd>GpStop<cr>", keymapOptions("Stop"))
+      vim.keymap.set({"n", "i", "v", "x"}, "<C-g>n", "<cmd>GpNextAgent<cr>", keymapOptions("Next Agent"))
+
+      -- Custom commands
+      vim.keymap.set({"n"}, "<C-g>s", "<cmd>%GpCheckGrammar<cr>", keymapOptions("Grammar Check File"))
+      vim.keymap.set({"n"}, "<C-g>S", "<cmd>%GpCheckGrammar gpt-4<cr>", keymapOptions("Grammar Check File with GPT4"))
+      vim.keymap.set({"v"}, "<C-g>s", ":<C-u>'<,'>GpCheckGrammar<cr>", keymapOptions("Grammar Check File"))
+      vim.keymap.set({"v"}, "<C-g>S", ":<C-u>'<,'>GpCheckGrammar gpt-4<cr>", keymapOptions("Grammar Check File with GPT4"))
+
+    end,  -- config
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et fdm=marker fmr={,} nofen
