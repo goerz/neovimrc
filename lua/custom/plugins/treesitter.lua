4d3169164e9a33c6b17fbf876f0369c7802c8d0e
Michael Goerz <mail@michaelgoerz.net>
2024-07-01 14:10:47 -0400

    Make sure treesitter is installed for Julia
    Apart from the improved syntax highlighting, this also seems to fix
    issues with indentation.

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'julia', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       auto_install = false,
       highlight = {
         enable = true,
