1d9dcb6f7279c4b425ab6b7225e17d087f759e10
Michael Goerz <mail@michaelgoerz.net>
2025-01-13 09:14:31 -0500

    Update Julia LSP wrapper

--- helpers/julia_languageserver.jl
+++ helpers/julia_languageserver.jl
@@ -1,37 +1,25 @@
-# Load LanguageServer.jl:
-# Attempt to load from ~/.julia/environments/nvim-lspconfig
-# with the regular load path as a fallback
-ls_install_path = joinpath(
-  get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")),
-  "environments",
-  "nvim-lspconfig"
-)
-if isdir(ls_install_path)
-    pushfirst!(LOAD_PATH, ls_install_path)
-else
-    @warn "The LS install path $ls_install_path does not exist"
-end
-
-
+# We load LanguageServer from the default environment. Previously, we would
+# use a dedicated environment, but that makes it more difficult to get things
+# working correctly when there are multiple versions of Julia installed.
 using LanguageServer
-
-isdir(ls_install_path) && popfirst!(LOAD_PATH)
+import Pkg
 
 depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
 
 
-function is_instantiated(path; project_filename="Project.toml")
+function is_instantiated(path)
     if isdir(path)
-        if project_filename == "JuliaProject.toml"
-            return isfile(joinpath(path, "JuliaManifest.toml"))
-        else
-            return isfile(joinpath(path, "Manifest.toml"))
+        for filename in Base.project_names
+            if is_instantiated(joinpath(path, filename))
+                return true
+            end
         end
     elseif isfile(path)
-        return is_instantiated(dirname(path); project_filename=basename(path))
-    else
-        return false
+        # https://discourse.julialang.org/t/determine-whether-a-project-has-been-instantiated/
+        env = Pkg.Types.EnvCache(path)
+        return Pkg.Operations.is_instantiated(env)
     end
+    return false
 end
 
 
@@ -40,6 +28,7 @@ function get_project_path()
     # Step 1: Tries an explicit project set by JULIA_PROJECT
     julia_project = get(ENV, "JULIA_PROJECT", nothing)
     if julia_project !== nothing && !isempty(julia_project)
+        @info "Using JULIA_PROJECT=$julia_project for LSP"
         path = Base.load_path_expand(julia_project)
         if isfile(path)
             path = dirname(path)
@@ -50,14 +39,18 @@ function get_project_path()
     # Step 2: Look for a Project.toml file in the current working directory and
     # up. If available, prefer environments in a `test` subfolder.
     project_toml = Base.current_project()
-    project_folder = dirname(project_toml)
-    if project_toml !== nothing
+    if isnothing(project_toml)
+        @warn "No current project"
+    else
+        project_folder = dirname(project_toml)
         # A test environment takes precedent over the main project environment
         test_folder = joinpath(project_folder, "test")
         if is_instantiated(test_folder)
+            @info "Use project_path=$test_folder (instantiated)"
             return test_folder
         else
             if is_instantiated(project_toml)
+                @info "Use project_path=$project_folder (instantiated)"
                 return project_folder
             else
                 @warn "Project folder $project_folder is not instantiated"
@@ -69,10 +62,12 @@ function get_project_path()
     first_load_path = get(Base.load_path(), 1, nothing)
     if first_load_path !== nothing
         if isfile(first_load_path)
-            return dirname(first_load_path)
+            project_path = dirname(first_load_path)
         else
-            return first_load_path
+            project_path = first_load_path
         end
+        @info "project_path = $project_path from load path (fallback)"
+        return project_path
     end
 
 end
